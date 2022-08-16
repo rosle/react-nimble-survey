bae8b5bc6f8379cd9012aeabe76ef33735c59170
Rossukhon Leagmongkol <rose.roko@gmail.com>
2022-08-16 09:51:46 +0700

    [#3] Set up coverage for component tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,7 @@
 import task from '@cypress/code-coverage/task';
 import { defineConfig } from 'cypress';
 
-const collectCodeCoverage = (on, config) => {
+const setupNodeEvents = (on, config) => {
   task(on, config);
 
   return config;
@@ -14,15 +14,36 @@ export default defineConfig({
     },
   },
   component: {
-    setupNodeEvents: collectCodeCoverage,
+    setupNodeEvents: setupNodeEvents,
     devServer: {
       framework: 'create-react-app',
       bundler: 'webpack',
+      // Need to instrument the application to collect code coverage.
+      // More info: https://glebbahmutov.com/blog/component-code-coverage/
+      webpackConfig: {
+        mode: 'development',
+        devtool: false,
+        module: {
+          rules: [
+            {
+              test: /\.(ts|tsx)$/,
+              exclude: /node_modules/,
+              use: {
+                loader: 'babel-loader',
+                options: {
+                  presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+                  plugins: ['istanbul'],
+                },
+              },
+            },
+          ],
+        },
+      },
     },
     specPattern: 'src/**/*.cy.{ts,tsx}',
   },
   e2e: {
-    setupNodeEvents: collectCodeCoverage,
+    setupNodeEvents: setupNodeEvents,
     baseUrl: 'http://localhost:3000',
     specPattern: 'cypress/e2e/**/*.cy.ts',
   },
