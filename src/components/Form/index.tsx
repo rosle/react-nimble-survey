4569c6dd90504b7b83decdcb67230ff0087e1f08
Rossukhon Leagmongkol <rose.roko@gmail.com>
2022-08-18 17:48:58 +0700

    [#5] Implement a Login error state

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,11 +1,40 @@
-import React, { FormHTMLAttributes } from 'react';
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
-interface FormProps extends FormHTMLAttributes<HTMLFormElement> {
+import Alert from 'components/Alert';
+import WarningIcon from 'components/Icon/Warning';
+
+interface FormProps extends React.FormHTMLAttributes<HTMLFormElement> {
   children: React.ReactNode;
 }
 
 const Form = ({ children }: FormProps) => {
-  return <form className="form">{children}</form>;
+  const { t } = useTranslation(['shared']);
+  const [isError] = useState(false);
+
+  const errorMessage = () => (
+    <>
+      <ul>
+        <li>Email can&apos;t be blank</li>
+        <li>Password can&apos;t be blank</li>
+      </ul>
+    </>
+  );
+
+  console.log(isError);
+
+  return (
+    <form className="form">
+      {isError && (
+        <div className="form__alert">
+          <Alert Icon={WarningIcon} title={t('shared:error')}>
+            {errorMessage()}
+          </Alert>
+        </div>
+      )}
+      {children}
+    </form>
+  );
 };
 
 export default Form;
