de0daaefd358bab771d5ca9266fa60beb965f24d
Rossukhon Leagmongkol <rose.roko@gmail.com>
2022-08-18 10:20:11 +0700

    [#4] Add tests for button

---
+++ src/components/Button/index.cy.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import Button, { buttonTestIds } from '.';
+
+describe('Button', () => {
+  it('renders a button with the custom label', () => {
+    const buttonLabel = 'Sign in';
+
+    cy.mount(<Button label={buttonLabel} />);
+
+    cy.findByTestId(buttonTestIds.button).should('be.visible').and('have.text', buttonLabel);
+  });
+
+  it('renders a primary and md button by default', () => {
+    cy.mount(<Button label="Sign in" />);
+
+    cy.findByTestId(buttonTestIds.button).should('have.class', 'btn--primary');
+  });
+
+  it('renders a secondary button if buttonStyle is secondary', () => {
+    cy.mount(<Button label="Sign in" buttonStyle="secondary" />);
+
+    cy.findByTestId(buttonTestIds.button).should('have.class', 'btn--secondary');
+  });
+
+  it('renders an sm button if buttonSize is sm', () => {
+    cy.mount(<Button label="Sign in" buttonSize="sm" />);
+
+    cy.findByTestId(buttonTestIds.button).should('have.class', 'btn--sm');
+  });
+});
---
+++ src/components/Button/index.test.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import { render, screen } from '@testing-library/react';
+
+import Button, { buttonTestIds } from '.';
+
+describe('Button', () => {
+  it('renders a button with the custom label', () => {
+    const buttonLabel = 'Sign in';
+
+    render(<Button label={buttonLabel} />);
+
+    const button = screen.getByTestId(buttonTestIds.button);
+
+    expect(button).toBeVisible();
+    expect(button).toHaveTextContent(buttonLabel);
+  });
+
+  it('renders a primary and md button by default', () => {
+    render(<Button label="Sign in" />);
+
+    const button = screen.getByTestId(buttonTestIds.button);
+
+    expect(button).toHaveClass('btn--primary');
+  });
+
+  it('renders a secondary button if buttonStyle is secondary', () => {
+    render(<Button label="Sign in" buttonStyle="secondary" />);
+
+    const button = screen.getByTestId(buttonTestIds.button);
+
+    expect(button).toHaveClass('btn--secondary');
+  });
+
+  it('renders an sm button if buttonSize is sm', () => {
+    render(<Button label="Sign in" buttonSize="sm" />);
+
+    const button = screen.getByTestId(buttonTestIds.button);
+
+    expect(button).toHaveClass('btn--sm');
+  });
+});
