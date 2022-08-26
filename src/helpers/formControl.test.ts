b9c17b2c61c16dab2090297debfb50ba3143cf8b
Rossukhon Leagmongkol <rose.roko@gmail.com>
2022-08-26 12:13:39 +0700

    [#4] Remove unnecessary describe

--- src/helpers/formControl.test.ts
+++ src/helpers/formControl.test.ts
@@ -1,29 +1,27 @@
 import { resolveInputIdFromName } from './formControl';
 
-describe('formControlHelper', () => {
-  describe('resolveInputIdFromName', () => {
-    describe('given an ID attribute is set', () => {
-      it('returns the given ID', () => {
-        const htmlAttributes = { id: 'MyInputA', name: 'input_a' };
+describe('resolveInputIdFromName', () => {
+  describe('given an ID attribute is set', () => {
+    it('returns the given ID', () => {
+      const htmlAttributes = { id: 'MyInputA', name: 'input_a' };
 
-        expect(resolveInputIdFromName(htmlAttributes)).toBe('MyInputA');
-      });
+      expect(resolveInputIdFromName(htmlAttributes)).toBe('MyInputA');
     });
+  });
 
-    describe('given an ID attribute is NOT set', () => {
-      it('returns a camel-cased name', () => {
-        const htmlAttributes = { name: 'input_a' };
+  describe('given an ID attribute is NOT set', () => {
+    it('returns a camel-cased name', () => {
+      const htmlAttributes = { name: 'input_a' };
 
-        expect(resolveInputIdFromName(htmlAttributes)).toBe('inputA');
-      });
+      expect(resolveInputIdFromName(htmlAttributes)).toBe('inputA');
     });
+  });
 
-    describe('given both ID and name attributes are NOT set', () => {
-      it('returns undefined', () => {
-        const htmlAttributes = {};
+  describe('given both ID and name attributes are NOT set', () => {
+    it('returns undefined', () => {
+      const htmlAttributes = {};
 
-        expect(resolveInputIdFromName(htmlAttributes)).toBeUndefined();
-      });
+      expect(resolveInputIdFromName(htmlAttributes)).toBeUndefined();
     });
   });
 });
