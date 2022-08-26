365d861e406eb9320abe3216ab9d3e1dbe985294
Rossukhon Leagmongkol <rose.roko@gmail.com>
2022-08-26 15:47:24 +0700

    [#5] Add success case for login

--- src/screens/Login/index.cy.tsx
+++ src/screens/Login/index.cy.tsx
@@ -13,6 +13,20 @@ describe('LoginScreen', () => {
     cy.findByTestId(authLayoutTestIds.headerTitle).should('have.text', 'auth:heading.sign_in');
   });
 
+  describe('given the valid inputs', () => {
+    // TODO: Add expectation after form submit on issue#6
+    it('does NOT display the error', () => {
+      cy.mount(<LoginScreen />);
+
+      cy.findByTestId(loginScreenTestIds.loginEmail).type('rossukhon@nimblehq.co');
+      cy.findByTestId(loginScreenTestIds.loginPassWord).type('secret1234');
+
+      cy.findByTestId(loginScreenTestIds.loginSubmit).click();
+
+      cy.findByTestId(formTestIds.formError).should('not.exist');
+    });
+  });
+
   describe('given the INVALID inputs', () => {
     it('displays the errors', () => {
       cy.mount(<LoginScreen />);
--- src/screens/Login/index.test.tsx
+++ src/screens/Login/index.test.tsx
@@ -18,6 +18,26 @@ describe('LoginScreen', () => {
     expect(authLayoutHeaderTitle).toHaveTextContent('auth:heading.sign_in');
   });
 
+  describe('given the valid inputs', () => {
+    // TODO: Add expectation after form submit on issue#6
+    it('does NOT display the error', () => {
+      render(<LoginScreen />);
+
+      const emailInput = screen.getByTestId(loginScreenTestIds.loginEmail);
+      const passwordInput = screen.getByTestId(loginScreenTestIds.loginPassWord);
+      const submitButton = screen.getByTestId(loginScreenTestIds.loginSubmit);
+
+      fillInput(emailInput, 'rossukhon@nimblehq.co');
+      fillInput(passwordInput, 'secret1234');
+
+      userEvent.click(submitButton);
+
+      const formError = screen.queryByTestId(formTestIds.formError);
+
+      expect(formError).not.toBeInTheDocument();
+    });
+  });
+
   describe('given the INVALID inputs', () => {
     it('displays the errors', async () => {
       render(<LoginScreen />);
