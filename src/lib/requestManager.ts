68d7c39c9645a2ba4b57a082599699b4726e06a0
Rossukhon Leagmongkol <rose.roko@gmail.com>
2022-08-30 10:14:13 +0700

    [#7] Clean up TODOs

--- src/lib/requestManager.ts
+++ src/lib/requestManager.ts
@@ -3,7 +3,6 @@ import { camelizeKeys, decamelizeKeys } from 'humps';
 
 import ApiError from './errors/ApiError';
 
-// TODO: Coverage is not 100% for transformRequest and transformResponse, recheck when adding integration test on issue#6
 export const defaultOptions: AxiosRequestConfig = {
   baseURL: process.env.REACT_APP_API_BASE_URL,
   responseType: 'json',
