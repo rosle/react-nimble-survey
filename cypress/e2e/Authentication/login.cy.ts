3c4979cb2db627423ecef2ba4565cfd641e26231
Rossukhon Leagmongkol <rose.roko@gmail.com>
2022-08-30 18:08:31 +0700

    [#6] Add Cypress e2e tests

--- cypress/e2e/Authentication/login.cy.ts
+++ cypress/e2e/Authentication/login.cy.ts
@@ -1,3 +1,26 @@
+// TODO: How to reuse the component's test ids
+const loginScreenTestIds = {
+  loginForm: 'login-form',
+  loginEmail: 'login-form__input-email',
+  loginPassWord: 'login-form__input-password',
+  loginSubmit: 'login-form__button-submit',
+};
+
 describe('Login', () => {
-  // TODO: Implement login e2e test after integrated with the API
+  context('given a valid credential', () => {
+    it('redirects to the Home page', () => {
+      cy.visit('sign_in');
+
+      cy.findByTestId(loginScreenTestIds.loginEmail).type('rossukhon@nimblehq.co');
+      cy.findByTestId(loginScreenTestIds.loginPassWord).type('secret22');
+
+      cy.intercept('POST', '/api/v1/oauth/token', { statusCode: 200, fixture: 'login_success' });
+
+      cy.findByTestId(loginScreenTestIds.loginSubmit).click();
+
+      cy.location().should((location) => {
+        expect(location.pathname).to.eq('/')
+      });
+    });
+  });
 });
