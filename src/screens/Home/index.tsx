e8fa6e2e9b88f0d61bec405b4d9fff983561c934
Rossukhon Leagmongkol <rose.roko@gmail.com>
2022-09-12 16:53:13 +0700

    [#15] Update sass and add TODO

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import DefaultLayout from 'components/Layout/Default';
 
+// TODO: Update the home page later on #17
 const HomeScreen = () => {
   return <DefaultLayout>This is the home page content</DefaultLayout>;
 };
